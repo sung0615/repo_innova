5b1e4e4d09606c37e4bb70cf8f7698cc6888daa7
sung0615 <kk1hhh1fff1@naver.com>
2025-02-27 15:42:35 +0900

    first

---
+++ innova/src/main/java/com/aa1133aa/demo/InnovaApplication.java
@@ -0,0 +1,13 @@
+package com.aa1133aa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaApplication.class, args);
+	}
+
+}
---
+++ innova/src/test/java/com/aa1133aa/demo/InnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aa1133aa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
